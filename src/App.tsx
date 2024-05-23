2d3f036ca51b4b26d088c011fc65372187a97dd1
goparklubanic <nugroho.redbuff@gmail.com>
2024-05-23 15:51:39 +0700

    project cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+// import { Redirect, Route } from 'react-router-dom'; <-origin
+// import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'; <-origin
+// import { IonReactRouter } from '@ionic/react-router'; <-origin
+// import Home from './pages/Home'; <-origin
+// origin = bawaan dari initial ionic project
+
+import { 
+  IonApp, 
+  setupIonicReact, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonContent, 
+  IonGrid, 
+  IonCol, 
+  IonInput, 
+  IonItem, 
+  IonLabel, 
+  IonRow, 
+  IonButton, 
+  IonIcon
+} from '@ionic/react';
+import { scaleOutline, refreshOutline } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,7 +47,10 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+/* import '@ionic/react/css/palettes/dark.system.css'; */
+import '@ionic/react/css/palettes/high-contrast.css'
+/* import custom.css */
+import './imt.css';
 
 /* Theme variables */
 import './theme/variables.css';
@@ -37,16 +59,45 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Masa Index Tubuh</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className='ion-padding'>
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonItem>
+              <IonLabel position='floating'>Tinggi Badan (meter)</IonLabel>
+              <IonInput></IonInput>
+            </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonItem>
+              <IonLabel position='floating'>Berat Badan (kg)</IonLabel>
+              <IonInput></IonInput>
+            </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonButton className='w-full'>
+              <IonIcon slot='start' icon={scaleOutline} />
+              Hitung
+            </IonButton>
+          </IonCol>
+          <IonCol>
+            <IonButton className='w-full'>
+              <IonIcon slot='start' icon={refreshOutline} />
+              Reset
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
   </IonApp>
 );
 
